5883eff79c83af03b8b27bcca6f6104a2cfd7483
Haile <hailecst@gmail.com>
2026-01-15 23:29:24 +0300

    isolating application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -38,31 +38,11 @@ func main() {
 	godotenv.Load() // Load .env file
 	addr := os.Getenv("SNIPPETBOX_ADDR")
 
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" dir.
-	// Note that the path given to the http.Dir function is relative to the project
-	// dir root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// Swap the route declarations to use the application struct's methods as the
-	// handler function.
-	// Register other application routes as normal.
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	// Use the Info() method to log the starting server message at Info severity
 	// (along with the listen address as an attribute).
 	logger.Info("starting server", "addr", addr)
 
-	err := http.ListenAndServe(addr, mux)
+	err := http.ListenAndServe(addr, app.routes())
 
 	// And we also use the Error() method to log any error message returned by
 	// http.ListenAndServer() at Error severity (with no additional attributes),
