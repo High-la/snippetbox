36cb70e921ec15472c571739f8f01d12c4fac081
Haile <hailecst@gmail.com>
2026-01-15 22:19:27 +0300

    using structured logging using the Go slog package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
@@ -9,13 +9,18 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
+
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	// os.Getenv() only reads from already setted system environment variables.
 	// so we use the godotenv package to read the .env file and set the
 	// environment variables before we call os.Getenv().
 	godotenv.Load() // Load .env file
 	addr := os.Getenv("SNIPPETBOX_ADDR")
+
+	mux := http.NewServeMux()
 
 	// Create a file server which serves files out of the "./ui/static" dir.
 	// Note that the path given to the http.Dir function is relative to the project
@@ -33,7 +38,15 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting server on %s", addr)
+	// Use the Info() method to log the starting server message at Info severity
+	// (along with the listen address as an attribute).
+	logger.Info("starting server", "addr", addr)
+
 	err := http.ListenAndServe(addr, mux)
-	log.Fatal(err)
+
+	// And we also use the Error() method to log any error message returned by
+	// http.ListenAndServer() at Error severity (with no additional attributes),
+	// and then call os.Exit(1) to terminate the application with exit code 1.
+	logger.Error(err.Error())
+	os.Exit(1)
 }
