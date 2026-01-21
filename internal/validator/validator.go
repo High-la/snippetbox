b1ea0220be88ed6bc2aeed59d3b995c5f4e365be
Haile <hailecst@gmail.com>
2026-01-21 17:19:30 +0300

    user login

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -9,13 +9,26 @@ import (
 
 // Define a new Validator struct which contains a map of validation error messages
 // for our form fields.
+
+// Add a new NonFieldErrors []string field to the struct, which we will use to
+// hold any validation errors which are not related to a specific form field.
 type Validator struct {
-	FieldErrors map[string]string
+	FieldErrors    map[string]string
+	NonFieldErrors []string
 }
 
 // Valid() returns true if the FieldErrors map doesn't contain any entries.
+
+// Update the Valid() method to also check that the NonFieldErrors slice is
+// empty
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+// Create an AddNonFieldError() helper for adding error messages to the new
+// NonFieldErrors slice.
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 // AddFieldError() adds an error message to the FieldErrors map (so as long as no
